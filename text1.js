e8d91457a97c84cab211c34af25e0f8a79ce7fe2
Rdennett3 <r.dennett3@gmail.com>
2015-09-14 16:00:06 -0700

    header changes color, button changes color, temp converter works

--- text1.js
+++ text1.js
@@ -1,25 +1,37 @@
+var count = 1;
+
 function updateHeader(){
     var newHeader=document.getElementById("mytextbox").value;
-    var greeting= ("hello there ")
+    var greeting= ("hello there, ");
     var oldHeader= document.getElementById("header");
+    var property = document.getElementById("btn");
 
-    // alert(newHeader);
+
+
+
+    if (count == 0) {
+        property.style.backgroundColor = "C08F0C"
+        count = 1;
+    }
+    else {
+        property.style.backgroundColor = "#867F6C"
+        count = 0;
+    }
+    //alert(newHeader);
     window.confirm("Are you sure you want to do this?");
+
+
     oldHeader.innerHTML= greeting.concat(newHeader);
-    document.getElementById("header").style.color="black"
+    oldHeader.style.color = "Blue";
+
+
+
 
 }
 
 
-var count = 1;
-function setColor(btn, color) {
-    var property = document.getElementById(btn);
-    if (count == 0) {
-        property.style.backgroundColor = "#FFFFFF"
-        count = 1;
-    }
-    else {
-        property.style.backgroundColor = "black"
-        count = 0;
-    }
+function convert(){
+  var input=document.getElementById("myothertextbox").value;
+  var output = (input - 32) * (5 / 9);
+  window.alert("The answer is: " + output);
 }
