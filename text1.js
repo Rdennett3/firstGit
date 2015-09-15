740e103a2fcb0d28cfc0e76d29c1a771c23c4820
Rdennett3 <r.dennett3@gmail.com>
2015-09-15 13:28:35 -0700

    prettyquote

--- text1.js
+++ text1.js
@@ -1,5 +1,7 @@
 var count = 1;
 
+
+
 function updateHeader(){
     var newHeader=document.getElementById("mytextbox").value;
     var greeting= ("hello there, ");
@@ -34,4 +36,19 @@ function convert(){
   var input=document.getElementById("myothertextbox").value;
   var output = (input - 32) * (5 / 9);
   window.alert("The answer is: " + output);
+}
+
+
+function prettyQuot(){
+  var x=document.getElementById("num").value;
+  var y=document.getElementById("den").value;
+  var mod=(x%y);
+  var quot=(x/y);
+  quot=quot.toString();
+  decimal=quot.indexOf(".");
+  newquot=quot.substring(0,decimal);
+  if (newquot=="") {
+    (newquot = quot);
+  }
+  window.alert(newquot + "r" + mod.toString());
 }
