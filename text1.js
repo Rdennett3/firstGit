2563b6d6d136b4c7855b2fece1d934d8f068889a
Rdennett3 <r.dennett3@gmail.com>
2015-09-16 16:47:20 -0700

    lots of new stuff

--- text1.js
+++ text1.js
@@ -1,4 +1,67 @@
 $(document).ready(function() {
+
+  var count = 1;
+  var hockeyTeams=["Boston Bruins", "New York Rangers", "Chicago Blackhawks"];
+  var currentTeam=0;
+  var badTeams=["a", "b", "c"];
+  var lineCounter=100;
+  var song1=" bottles of beer on the wall ";
+  var song2=" bottles of beer, take one down, pass it around ";
+  var tall=["Space Mountain", "Big Thunder Mountain", "Indiana Jones Adventure"];
+  var short=["Haunted Mansion", "Jungle Cruise", "Mad Tea Party"];
+
+//  function(string)(
+  //  alert("hi");
+//)
+
+  function singSong(){
+    while ( lineCounter > 0 ){
+      lineCounter--;
+      $("ul#beer").append("<li>" + lineCounter + song1 + lineCounter + song2 + (lineCounter-1) + song1 +"</li>");
+
+      }
+    }
+
+  function listTeams(){
+    badTeams.forEach(function(team){
+      $("ul#bad-teams").prepend("<li>"+team+"</li>");
+      });
+    }
+
+function checkHeight(x){
+    x=x.toString();
+    x=x.split(",");
+    var feet=x[0];
+    var inches=x[1];
+    feet=parseInt(feet);
+    inches=parseInt(inches);
+    feet=(feet * 12);
+    inches=(feet + inches);
+    alert(inches);
+    displayRides(inches);
+
+}
+
+function displayRides(y){
+
+      short.forEach(function(rides){
+        $("ul#height-list").prepend("<li>"+rides+"</li>");
+        });
+        if (y>40){
+          tall.forEach(function(rides){
+            $("ul#height-list").prepend("<li>"+rides+"</li>");
+            });
+        }
+}
+
+    $( "button#height-btn" ).click(function() {
+        var myHeight=$( "#getHeight" ).val();
+        //alert(myHeight);
+        checkHeight(myHeight);
+    });
+
+
+
   $("button#hello").click(function() {
     $('h1#demo').after('<h2>New subheading</h2>');
     $("ul#user").prepend("<li>Hello! <span class='clickable delete'></span></li>");
@@ -11,10 +74,37 @@ $(document).ready(function() {
     });
   });
   $(".toggle").click(function() {
-    $("#showing").fadeToggle("slow");
-    $("#hidden").fadeToggle("slow");
+    $("#showing").toggle();
+    $("#hidden").toggle();
     });
 
+
+$("button#team-btn").click(function(){
+
+    if(currentTeam < 3){
+      $("ul#teams").prepend("<li>"+ hockeyTeams[currentTeam]+"</li>");
+      currentTeam++;
+    }
+    else{
+      $("ul#teams").prepend("<li> Rebecca only know three teams. </li>");
+    }
+    $("ul#teams").children('li').first().click(function() {
+      alert('hi');
+    });
+  });
+
+$("button#array-btn").click(function() {
+        listTeams();
+      });
+
+
+
+
+$("img#myButton").click(function(){
+        alert("You shouldn't have done that");
+        singSong()
+
+      });
 
 
 
@@ -24,8 +114,6 @@ $(document).ready(function() {
 
 
 
-
-var count = 1;
 
 
 function updateHeader(){
@@ -56,6 +144,7 @@ function updateHeader(){
 
 
 }
+
 
 
 function convert(){
